c6abe34142d2e29a9d8addd3db1c0b26641511ab
crystalDf <chendong333@gmail.com>
2015-08-12 23:11:48 +0800

    Chapter 11 AnimationDemo full animation

--- app/src/main/java/com/star/animationdemo/MainActivity.java
+++ app/src/main/java/com/star/animationdemo/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,16 +15,54 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button alphaButton = (Button) findViewById(R.id.alpha_button);
         Button scaleButton = (Button) findViewById(R.id.scale_button);
+        Button transButton = (Button) findViewById(R.id.trans_button);
+        Button rotateButton = (Button) findViewById(R.id.rotate_button);
 
-        final TextView textView = (TextView) findViewById(R.id.text_view);
+        Button setButton = (Button) findViewById(R.id.set_button);
 
+        final ImageView imageView = (ImageView) findViewById(R.id.image);
+
+        final Animation alphaAnimation = AnimationUtils.loadAnimation(this, R.anim.alphaanim);
         final Animation scaleAnimation = AnimationUtils.loadAnimation(this, R.anim.scaleanim);
+        final Animation transAnimation = AnimationUtils.loadAnimation(this, R.anim.transanim);
+        final Animation rotateAnimation = AnimationUtils.loadAnimation(this, R.anim.rotateanim);
+
+        final Animation setAnimation = AnimationUtils.loadAnimation(this, R.anim.setanim);
+
+        alphaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.startAnimation(alphaAnimation);
+            }
+        });
 
         scaleButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textView.startAnimation(scaleAnimation);
+                imageView.startAnimation(scaleAnimation);
+            }
+        });
+
+        transButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.startAnimation(transAnimation);
+            }
+        });
+
+        rotateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.startAnimation(rotateAnimation);
+            }
+        });
+
+        setButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.startAnimation(setAnimation);
             }
         });
     }
