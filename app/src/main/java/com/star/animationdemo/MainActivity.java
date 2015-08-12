06e425109622d69604f77afa95642763f41a0033
crystalDf <chendong333@gmail.com>
2015-08-12 23:45:11 +0800

    Chapter 11 AnimationDemo dynamic

--- app/src/main/java/com/star/animationdemo/MainActivity.java
+++ app/src/main/java/com/star/animationdemo/MainActivity.java
@@ -3,8 +3,13 @@ package com.star.animationdemo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
+import android.view.animation.AnticipateOvershootInterpolator;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -31,6 +36,34 @@ public class MainActivity extends AppCompatActivity {
 
         final Animation setAnimation = AnimationUtils.loadAnimation(this, R.anim.setanim);
 
+        final AlphaAnimation alphaAnim = new AlphaAnimation(
+                0f, 1f
+        );
+
+        final ScaleAnimation scaleAnim = new ScaleAnimation(
+                0f, 1f, 0f, 1f,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f
+        );
+        scaleAnim.setDuration(2000);
+//        scaleAnim.setRepeatCount(4);
+//        scaleAnim.setRepeatMode(Animation.REVERSE);
+        scaleAnim.setFillAfter(true);
+        scaleAnim.setInterpolator(new AnticipateOvershootInterpolator());
+
+        final RotateAnimation rotateAnim = new RotateAnimation(
+                0, 720,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f
+        );
+
+        final AnimationSet animationSet = new AnimationSet(true);
+        animationSet.setDuration(3000);
+        animationSet.setFillAfter(true);
+        animationSet.setInterpolator(new AnticipateOvershootInterpolator());
+
+        animationSet.addAnimation(alphaAnim);
+        animationSet.addAnimation(scaleAnim);
+        animationSet.addAnimation(rotateAnim);
+
         alphaButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -41,7 +74,8 @@ public class MainActivity extends AppCompatActivity {
         scaleButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                imageView.startAnimation(scaleAnimation);
+//                imageView.startAnimation(scaleAnimation);
+                imageView.startAnimation(scaleAnim);
             }
         });
 
@@ -62,7 +96,8 @@ public class MainActivity extends AppCompatActivity {
         setButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                imageView.startAnimation(setAnimation);
+//                imageView.startAnimation(setAnimation);
+                imageView.startAnimation(animationSet);
             }
         });
     }
